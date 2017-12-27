663b1cc4f7faf6efd1532953a5324741ac1a8565
Christophe Pouliot <cristophepoug@gmail.com>
2017-12-27 09:43:31 -0800

    Supply commit author details on run. First working Lambda version

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import * as fs from 'fs'
 
 import installGit from 'lambda-git'
-import simpleGit from 'simple-git'
+import simpleGit from 'simple-git/promise'
 import path from 'path'
 
 // CONFIG
@@ -10,27 +10,53 @@ const _githubToken = process.env.GITHUB_TOKEN
 const _repoName = process.env.REPO_NAME
 const _commitFile = process.env.COMMIT_FILE
 
-const repoPath = path.join(__dirname, _repoName)
+const repoPath = path.join('/tmp', _repoName)
 const commitFilePath = path.join(repoPath, _commitFile)
 const repoURLWithToken = _rawRepoUrl.replace('github.com', `${_githubToken}@github.com`)
 
-// setup Git for our process
-installGit()
-fs.mkdirSync(repoPath)
-const git = simpleGit(repoPath).silent(true)
+function handler(event, context) {
+  let git = null
+  installGit()
+    .then(() => {
+      console.log('Making repoPath dir..')
+      return fs.mkdir(repoPath)
+    })
+    .then(() => {
+      console.log('Init simple git..')
+      git = simpleGit(repoPath)
+    })
+    .then(() => {
+      console.log('Cloning..')
+      return git.clone(repoURLWithToken, repoPath)
+    })
+    .then(() => {
+      console.log('Appending to file..')
+      return fs.appendFile(commitFilePath, '!')
+    })
+    .then(() => {
+      console.log('Git add..')
+      return git.add('./*')
+    })
+    .then(() => {
+      console.log('Git commit..')
+      return git.raw([
+        '-c',
+        'user.name=\'Christophe Pouliot\'',
+        '-c',
+        'user.email=\'cristophepoug@gmail.com\'',
+        'commit',
+        '-m',
+        '\'Github buff commit\'',
 
-async function handler(event, context) {
-  try {
-    await git.clone(repoURLWithToken, repoPath)
-    await fs.appendFile(commitFilePath, '!')
-    await git.add('./*')
-    await git.commit('Github buff commit')
-    await git.push(['origin', 'master'])
-  } catch (e) {
-    context.fail(e)
-  }
-  context.succeed()
+      ])
+    })
+    .then(() => {
+      console.log('Git push..')
+      return git.push(['origin', 'master'])
+    })
+    .then(() => context.succeed())
+    .catch(e => context.fail(e))
 }
 
-export default handler
+exports.handler = handler
 
